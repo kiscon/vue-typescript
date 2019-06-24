5afe6f82faeea19117d231f6d8a84a3f81d0d9d7
kiscon <kiscon@163.com>
2019-06-25 00:21:59 +0800

    配置vue.config引入全局样式

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+// https://github.com/vuejs/vue-cli/blob/dev/docs/zh/config/README.md
+// https://www.cnblogs.com/ginkgo-leaves/p/10718478.html
+const path = require('path')
 module.exports = {
 	publicPath: '/',
 	outputDir: process.env.outputDir || 'dist',
@@ -5,15 +8,6 @@ module.exports = {
 	runtimeCompiler: false,
 	productionSourceMap: false,
 	lintOnSave: false,
-	css: {
-		// modules: true,
-		sourceMap: false,
-		loaderOptions: {
-			less: {
-				data: `@import '~@/common/less/reset.less';`
-			}
-		}
-	},
 	devServer: {
 		host: 'localhost',
 		https: false,
@@ -29,9 +23,9 @@ module.exports = {
 		}
 	},
 	chainWebpack: config => {
-	// https://github.com/vuejs/vue-cli/issues/1104
-	// typescript building error when import .vue file with separate .ts file, 即在 ts 文件中无法引入 .vue 组件;
-	// 下面的代码解决上面这个 bug, 解决方法来源: https://github.com/Realytics/fork-ts-checker-webpack-plugin/issues/111
+		// https://github.com/vuejs/vue-cli/issues/1104
+		// typescript building error when import .vue file with separate .ts file, 即在 ts 文件中无法引入 .vue 组件;
+		// 下面的代码解决上面这个 bug, 解决方法来源: https://github.com/Realytics/fork-ts-checker-webpack-plugin/issues/111
 		config.plugins.delete('fork-ts-checker')
 		config.module
 			.rule('ts')
@@ -41,5 +35,16 @@ module.exports = {
 					'transpileOnly': false
 				}
 			})
+		const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+		types.forEach(type =>
+			config.module
+				.rule('less')
+				.oneOf(type)
+				.use('style-resource')
+				.loader('style-resources-loader')
+				.options({
+					patterns: [path.join(__dirname, './src/common/less/reset.less')]
+				})
+		)
 	}
 }
